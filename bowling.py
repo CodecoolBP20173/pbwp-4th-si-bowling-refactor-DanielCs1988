df185e2a21a70917d3227c81eb9c24b314c5f1eb
Daniel Császár <daniel.cs20@gmail.com>
2017-11-28 22:21:05 +0100

    Renamed get_value and char to grade_roll and roll for further name clarification.

--- bowling.py
+++ bowling.py
@@ -1,43 +1,43 @@
-def score(game):
+def score(rolls):
     result = 0
     frame = 1
     in_first_half = True
 
-    for i in range(len(game)):
-        if game[i] == '/':
+    for i in range(len(rolls)):
+        if rolls[i] == '/':
             result += 10 - last
         else:
-            result += get_value(game[i])
-        if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i + 1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i + 1])
-                if game[i + 2] == '/':
-                    result += 10 - get_value(game[i + 1])
+            result += grade_roll(rolls[i])
+        if frame < 10 and grade_roll(rolls[i]) == 10:
+            if rolls[i] == '/':
+                result += grade_roll(rolls[i + 1])
+            elif rolls[i] == 'X' or rolls[i] == 'x':
+                result += grade_roll(rolls[i + 1])
+                if rolls[i + 2] == '/':
+                    result += 10 - grade_roll(rolls[i + 1])
                 else:
-                    result += get_value(game[i + 2])
-        last = get_value(game[i])
+                    result += grade_roll(rolls[i + 2])
+        last = grade_roll(rolls[i])
         if not in_first_half:
             frame += 1
         if in_first_half == True:
             in_first_half = False
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if rolls[i] == 'X' or rolls[i] == 'x':
             in_first_half = True
             frame += 1
     return result
 
 
-def get_value(char):
-    if char in '123456789':
-        return int(char)
+def grade_roll(roll):
+    if roll in '123456789':
+        return int(roll)
 
-    if char in 'xX/':
+    if roll in 'xX/':
         return 10
 
-    if char == '-':
+    if roll == '-':
         return 0
 
     raise ValueError()
